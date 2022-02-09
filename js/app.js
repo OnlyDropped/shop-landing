17c7c5a8a7b7f0e17f94cd6d0fccb603d198a187
OnlyDropped <asheburenkov@yandex.ru>
2022-02-09 18:25:54 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$('.slider').slick({
+  cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 2500,
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/icon/arrow-prev.svg" alt=""></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="img/icon/arrow-next.svg" alt=""></button>',
+  touchThreshold: 100
+  //dots: false,
+  //arrows: true,
+});
+
+
+
