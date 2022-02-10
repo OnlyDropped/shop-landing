c70808505fe97450a76ff3e2c0739041eaf8b290
OnlyDropped <asheburenkov@yandex.ru>
2022-02-10 18:10:39 +0300

    added section our in index.html, change slider

--- js/app.js
+++ js/app.js
@@ -6,9 +6,11 @@ $('.slider').slick({
   speed: 2500,
   prevArrow: '<button type="button" class="slick-prev"><img src="img/icon/arrow-prev.svg" alt=""></button>',
   nextArrow: '<button type="button" class="slick-next"><img src="img/icon/arrow-next.svg" alt=""></button>',
-  touchThreshold: 100
-  //dots: false,
-  //arrows: true,
+  touchThreshold: 100,
+  dots: true,
+  arrows: true,
+  lazyLoad: 'ondemand',
+  dotsClass: 'slick-dots'
 });
 // код со стека
 //https://ru.stackoverflow.com/questions/1286386/%D0%9F%D0%BE%D1%87%D0%B5%D0%BC%D1%83-%D0%B4%D0%B5%D1%80%D0%B3%D0%B0%D0%B5%D1%82%D1%81%D1%8F-%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82-%D0%BF%D1%80%D0%B8-%D0%BD%D0%B0%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B8
