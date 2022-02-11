046e523570b9db36300c777c5db898724a56bbc9
OnlyDropped <asheburenkov@yandex.ru>
2022-02-11 18:31:23 +0300

    added change section sales

--- js/app.js
+++ js/app.js
@@ -15,5 +15,12 @@ $('.slider').slick({
 // код со стека
 //https://ru.stackoverflow.com/questions/1286386/%D0%9F%D0%BE%D1%87%D0%B5%D0%BC%D1%83-%D0%B4%D0%B5%D1%80%D0%B3%D0%B0%D0%B5%D1%82%D1%81%D1%8F-%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82-%D0%BF%D1%80%D0%B8-%D0%BD%D0%B0%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B8
 
-
+$('.sales-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/icon/chevron-prev.svg" alt=""></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="img/icon/arrow-next.svg" alt=""></button>',
+  touchThreshold: 100,
+});
 
