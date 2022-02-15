ecb286b6f8086e34fa680f130256d2da7f14fe6d
OnlyDropped <asheburenkov@yandex.ru>
2022-02-15 17:54:59 +0300

    added change media index.html

--- js/app.js
+++ js/app.js
@@ -10,7 +10,16 @@ $('.slider').slick({
   dots: true,
   arrows: true,
   lazyLoad: 'ondemand',
-  dotsClass: 'slick-dots'
+  dotsClass: 'slick-dots', 
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
 // код со стека
 //https://ru.stackoverflow.com/questions/1286386/%D0%9F%D0%BE%D1%87%D0%B5%D0%BC%D1%83-%D0%B4%D0%B5%D1%80%D0%B3%D0%B0%D0%B5%D1%82%D1%81%D1%8F-%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82-%D0%BF%D1%80%D0%B8-%D0%BD%D0%B0%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B8
